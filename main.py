176e871246d03a6131dc2ac0db60950bdafc2f91
DamirNR <fordomenic@gmail.com>
2024-07-19 12:21:10 +0300

    Поняенял наименование сервиса и поставил задержку

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from fastapi import FastAPI
 import socket
+import time
 app = FastAPI(title="Task docker for OTUS")
+hostname=socket.gethostname()
+time.sleep (0.9)
 @app.get("/health/")
 def hello(name: str = ""):
-   return {"status": "OK"}
+   return {"status": "OK"}, hostname
