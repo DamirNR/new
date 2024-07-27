36541fb0e65d84c88e6e57232d879e4598c57bed
DamirNR <fordomenic@gmail.com>
2024-07-27 17:50:41 +0300

    Создал новый файл для задач на питоне

---
+++ Python.py
@@ -0,0 +1,11 @@
+some_line = 'Hello OTUS'
+ another_line = "Just some text"
+multiline = '''Hi!
+... This is an example of a 'multiline' text
+... Thanks, bye!'''
+some_line
+'Hello OTUS'
+print(some_line)
+Hello OTUS
+print(repr(some_line))
+'Hello OTUS'
